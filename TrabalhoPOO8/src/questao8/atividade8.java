e1def95f2c8707452d16c01060a8b8a37484361f
Thcordeiro <112040382+Thcordeiro@users.noreply.github.com>
2022-10-30 16:23:22 -0300

    Update atividade8.java

--- TrabalhoPOO8/src/questao8/atividade8.java
+++ TrabalhoPOO8/src/questao8/atividade8.java
@@ -6,17 +6,17 @@ public class atividade8 {
 	public static void main(String[] args) {
 		int n,k
 		Scanner scanf=new Scanner(System.in);
-		final int li=2;
-		final int co=5;
+		final int linha=2;
+		final int coluna=5;
 		double M[][]=new double [li][co];
 		for(n=0;<li;++) {
-			for(k=0;<co;k++) {
-			System.out.println("Informe o numero:");
+			for(k=0;<coluna;k++) {
+			System.out.println("Digite um numero:");
 			M[n][k]=scanf.nextDouble();	
 			
 			}
 		}
-			 for(int l=0;l<li;l++) {
+			 for(int n=0;n<linha;n++) {
 				for(int k=0;k<co;k++) {
 					System.out.print("Matriz["+l+"]["+k+"]= "+M[l][k]+"\n");
 				
